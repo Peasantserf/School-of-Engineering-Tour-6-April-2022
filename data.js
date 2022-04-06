5f26b28e02203ddaf10d16d1e2a28a6f4c5676fb
Peasantserf <39843640+Peasantserf@users.noreply.github.com>
2022-04-06 13:46:55 +0800

    Edits

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-along-engineering-drive-1",
-      "name": "Along Engineering Drive 1",
+      "id": "0-beginning-and-end",
+      "name": "Beginning and End",
       "levels": [
         {
           "tileSize": 256,
@@ -22,133 +22,214 @@ var APP_DATA = {
           "size": 2048
         }
       ],
-      "faceSize": 1440,
+      "faceSize": 1824,
       "initialViewParameters": {
-        "yaw": 0.34135655892862715,
-        "pitch": -0.08166346332907715,
-        "fov": 1.3808549253827036
+        "yaw": 0.4416995845009062,
+        "pitch": 0.23539358257798426,
+        "fov": 1.35820747241737
       },
       "linkHotspots": [
         {
-          "yaw": -0.023983242596303356,
-          "pitch": -0.1880611330894162,
-          "rotation": 6.283185307179586,
-          "target": "1-engineering-auditorium"
-        }
-      ],
-      "infoHotspots": [
-        {
-          "yaw": -0.15530798442359917,
-          "pitch": 0.29997802147454955,
-          "title": "Engineering<br>",
-          "text": "asdasd"
-        }
-      ]
-    },
-    {
-      "id": "1-engineering-auditorium",
-      "name": "Engineering Auditorium",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 1440,
-      "initialViewParameters": {
-        "yaw": 0.23763329046385095,
-        "pitch": -0.17260686567283656,
-        "fov": 1.3808549253827036
-      },
-      "linkHotspots": [
-        {
-          "yaw": -0.07572712641168877,
-          "pitch": -0.01517120973657704,
-          "rotation": 0.7853981633974483,
-          "target": "2-engineering-auditorium-atrium"
-        },
-        {
-          "yaw": -1.6548856510162953,
-          "pitch": -0.11372490199983609,
-          "rotation": 3.9269908169872414,
-          "target": "0-along-engineering-drive-1"
-        }
-      ],
-      "infoHotspots": [
-        {
-          "yaw": -0.09244455346576785,
-          "pitch": 0.14666666092875857,
-          "title": "Title",
-          "text": "Text"
-        },
-        {
-          "yaw": -1.895772781808187,
-          "pitch": 0.2370359564637745,
-          "title": "Title",
-          "text": "Text"
-        }
-      ]
-    },
-    {
-      "id": "2-engineering-auditorium-atrium",
-      "name": "Engineering Auditorium Atrium",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 1440,
-      "initialViewParameters": {
-        "yaw": -0.8109068216958519,
-        "pitch": -0.04454388224215933,
-        "fov": 1.3808549253827036
-      },
-      "linkHotspots": [
-        {
-          "yaw": -3.135599380918194,
-          "pitch": 0.0573338697143857,
+          "yaw": 1.1430533894916,
+          "pitch": 0.3760845966737314,
           "rotation": 0,
-          "target": "1-engineering-auditorium"
+          "target": "1-villa-resort"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": -1.1567728209528667,
-          "pitch": 0.42516235976543015,
-          "title": "Title",
-          "text": "Text"
+          "yaw": 0.9774938241461797,
+          "pitch": 0.573282534054421,
+          "title": "Charter Ride<br>",
+          "text": "Ride our comfortable boat to the Villa Resort.<br>"
+        }
+      ]
+    },
+    {
+      "id": "1-villa-resort",
+      "name": "Villa Resort",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.1060623160827312,
+        "pitch": 0.24660652080518908,
+        "fov": 1.35820747241737
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.035375287400750466,
+          "pitch": 0.5856859779421715,
+          "rotation": 0,
+          "target": "3-restauraunt"
+        },
+        {
+          "yaw": 1.6304312631101796,
+          "pitch": 0.14290258300987801,
+          "rotation": 0,
+          "target": "2-beachjpg"
+        },
+        {
+          "yaw": -1.5818904574832295,
+          "pitch": 0.18786120768129244,
+          "rotation": 0,
+          "target": "0-beginning-and-end"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.619330369547205,
+          "pitch": 0.3380330781447842,
+          "title": "<div>Return to Marina</div>",
+          "text": "Time to head off.&nbsp; Your day trip has ended.<br>"
+        },
+        {
+          "yaw": -0.2285267185915565,
+          "pitch": 0.28218989520634175,
+          "title": "Lunch<br>",
+          "text": "Enjoy a buffet lunch in our 5* Restauraunt.<br>"
+        },
+        {
+          "yaw": 1.6775496162439048,
+          "pitch": 0.03943703974020707,
+          "title": "Beach",
+          "text": "Enjoy some time swimming or tanning.<br>"
+        }
+      ]
+    },
+    {
+      "id": "2-beachjpg",
+      "name": "Beach.jpg",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.46318353226004305,
+        "pitch": 0.2297930111517843,
+        "fov": 1.35820747241737
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3791359265698553,
+          "pitch": 0.04079868279299603,
+          "rotation": 0.7853981633974483,
+          "target": "1-villa-resort"
+        },
+        {
+          "yaw": -3.060726336625743,
+          "pitch": -0.109820887483016,
+          "rotation": 0,
+          "target": "1-villa-resort"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.315071769739344,
+          "pitch": 0.18401585900471495,
+          "title": "Return to the Villa<br>",
+          "text": "Use the map to find your way back to our Villa.<br>"
+        },
+        {
+          "yaw": 2.9743941183459697,
+          "pitch": -0.2710354910337305,
+          "title": "Secret Passage<br>",
+          "text": "Back to Villa.<br>"
+        }
+      ]
+    },
+    {
+      "id": "3-restauraunt",
+      "name": "Restauraunt",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1575,
+      "initialViewParameters": {
+        "yaw": 2.552208390800944,
+        "pitch": 0.474507736948679,
+        "fov": 1.35820747241737
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.5813625787348187,
+          "pitch": 0.12690642670613173,
+          "rotation": 0,
+          "target": "2-beachjpg"
+        },
+        {
+          "yaw": -0.6236076393185392,
+          "pitch": 0.5800934073791755,
+          "rotation": 5.497787143782138,
+          "target": "1-villa-resort"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.6358042768903616,
+          "pitch": 0.3130433428495234,
+          "title": "Villa<br>",
+          "text": "Return to the Villa for some rest.<br>"
+        },
+        {
+          "yaw": -2.7733483822236966,
+          "pitch": -0.0735197955149598,
+          "title": "Beach<br>",
+          "text": "<div>Let's return to the beach.&nbsp; Rest in the sun.&nbsp; You deserve it.</div><div><br></div>"
         }
       ]
     }
   ],
-  "name": "Faculty of Engineering 6 April 2022",
+  "name": "Day Trip to Paradise",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": false,
