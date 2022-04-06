3daa204da0b46227411fd9bbc13dbf93eb2baa64
Peasantserf <39843640+Peasantserf@users.noreply.github.com>
2022-04-06 10:52:42 +0800

    First Commit

---
+++ data.js
@@ -0,0 +1,158 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-along-engineering-drive-1",
+      "name": "Along Engineering Drive 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.34135655892862715,
+        "pitch": -0.08166346332907715,
+        "fov": 1.3808549253827036
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.023983242596303356,
+          "pitch": -0.1880611330894162,
+          "rotation": 6.283185307179586,
+          "target": "1-engineering-auditorium"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.15530798442359917,
+          "pitch": 0.29997802147454955,
+          "title": "Engineering<br>",
+          "text": "asdasd"
+        }
+      ]
+    },
+    {
+      "id": "1-engineering-auditorium",
+      "name": "Engineering Auditorium",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.23763329046385095,
+        "pitch": -0.17260686567283656,
+        "fov": 1.3808549253827036
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.07572712641168877,
+          "pitch": -0.01517120973657704,
+          "rotation": 0.7853981633974483,
+          "target": "2-engineering-auditorium-atrium"
+        },
+        {
+          "yaw": -1.6548856510162953,
+          "pitch": -0.11372490199983609,
+          "rotation": 3.9269908169872414,
+          "target": "0-along-engineering-drive-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.09244455346576785,
+          "pitch": 0.14666666092875857,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.895772781808187,
+          "pitch": 0.2370359564637745,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-engineering-auditorium-atrium",
+      "name": "Engineering Auditorium Atrium",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.8109068216958519,
+        "pitch": -0.04454388224215933,
+        "fov": 1.3808549253827036
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.135599380918194,
+          "pitch": 0.0573338697143857,
+          "rotation": 0,
+          "target": "1-engineering-auditorium"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.1567728209528667,
+          "pitch": 0.42516235976543015,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Faculty of Engineering 6 April 2022",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
